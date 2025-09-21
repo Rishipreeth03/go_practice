a6f8178ec8a35354e4326f8b828dc5e4b5a4dc4a
Rishipreeth <rpoket47@gmail.com>
2025-09-21 11:54:15 +0530

    ongoing flow control

---
+++ 1_helloworld/README.md
@@ -0,0 +1,15 @@
+## You can use 2 command 
+```
+ go build main.go
+
+ .\main.exe
+
+ ```
+
+or 
+
+## do it in single command 
+
+```
+go run main.go
+```
---
+++ 2_simplevalues/simplevalue.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello")
+	fmt.Println(true)
+	fmt.Println(10.5)
+	var name string = "hello"
+	fmt.Println(name)
+	//youcan also declare in short hand
+	city := "Hyderabad"
+	fmt.Println(city, name)
+	fmt.Println(city+name)
+}
