361189dfc1276caaf44bdf54b8c2eeb4c55807ae
Rishipreeth <rpoket47@gmail.com>
2025-12-28 15:57:49 +0530

    Added students list

--- Api_Handler/internal/http/handlers/student/student.go
+++ Api_Handler/internal/http/handlers/student/student.go
@@ -68,3 +68,17 @@ func GetById(storage storage.Storage) http.HandlerFunc {
 		response.WriteJson(w, http.StatusOK, studentId)
 	}
 }
+
+func GetList(storage storage.Storage) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		slog.Info("getting all the students")
+
+		studentList, err := storage.GetStudentList()
+		if err != nil {
+			slog.Error("Error getting student list", slog.String("error", err.Error()))
+			response.WriteJson(w, http.StatusInternalServerError, response.GeneralError(err))
+			return
+		}
+		response.WriteJson(w, http.StatusOK, studentList)
+	}
+}
--- Api_Handler/internal/storage/storage.go
+++ Api_Handler/internal/storage/storage.go
@@ -5,4 +5,5 @@ import "github.com/Rishipreeth03/Api_Handler/Api_Handler/internal/types"
 type Storage interface {
 	CreateStudent(name string, email string, age int) (int64, error)
 	GetStudentByID(id int64) (types.Student, error)
+	GetStudentList() ([]types.Student, error)
 }
